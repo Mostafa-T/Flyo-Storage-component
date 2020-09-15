3beb56dc1f10bba73a6cb52f9c40ab39bed3bbda
Mustafa-T <54773901+Mostafa-T@users.noreply.github.com>
2020-09-15 18:48:33 +0200

    First Commit.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+const progressBar = document.querySelector('.progress__bar');
+
+window.addEventListener('load', () => {
+
+  // Fill the progress bar when the document is loaded
+  progressBar.classList.add('active');
+});
